9eabb91c0175ea1f08cefbf2caf4dafda6995ad4
Jerry Bruwes <jbruwes@gmail.com>
2024-12-06 18:47:55 +0200

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import type { WorkerLanguageService } from "@volar/monaco/worker";
+import type { App } from "vue";
 
 import {
   activateAutoInsertion,
@@ -10,26 +11,35 @@ import * as languageConfigs from "@vues3/monaco-volar-worker/src/language-config
 import { editor, languages } from "monaco-editor-core";
 import EditorWorker from "monaco-editor-core/esm/vs/editor/editor.worker?worker";
 
-const getWorker = (workerId: string, label: string) =>
-  label === "vue" ? new VueWorker() : new EditorWorker();
-window.MonacoEnvironment = { getWorker };
-const languageId = ["vue", "javascript", "typescript", "css"];
-["vue", "js", "ts", "css"].forEach((value, index) => {
-  const id = languageId[index];
-  const extensions = [`.${value}`];
-  languages.register({ extensions, id });
-  languages.setLanguageConfiguration(
-    id,
-    languageConfigs[value as keyof typeof languageConfigs],
-  );
-});
-const [label] = languageId;
-const moduleId = "vs/language/vue/vueWorker";
-const getSyncUris = () => editor.getModels().map(({ uri }) => uri);
-const worker = editor.createWebWorker<WorkerLanguageService>({
-  label,
-  moduleId,
-});
-activateMarkers(worker, languageId, label, getSyncUris, editor);
-activateAutoInsertion(worker, languageId, getSyncUris, editor);
-registerProviders(worker, languageId, getSyncUris, languages).catch(() => {});
+import VMonacoVolar from "./VMonacoVolar.vue";
+
+export default {
+  install: (app: App) => {
+    const getWorker = (workerId: string, label: string) =>
+      label === "vue" ? new VueWorker() : new EditorWorker();
+    window.MonacoEnvironment = { getWorker };
+    const languageId = ["vue", "javascript", "typescript", "css"];
+    ["vue", "js", "ts", "css"].forEach((value, index) => {
+      const id = languageId[index];
+      const extensions = [`.${value}`];
+      languages.register({ extensions, id });
+      languages.setLanguageConfiguration(
+        id,
+        languageConfigs[value as keyof typeof languageConfigs],
+      );
+    });
+    const [label] = languageId;
+    const moduleId = "vs/language/vue/vueWorker";
+    const getSyncUris = () => editor.getModels().map(({ uri }) => uri);
+    const worker = editor.createWebWorker<WorkerLanguageService>({
+      label,
+      moduleId,
+    });
+    activateMarkers(worker, languageId, label, getSyncUris, editor);
+    activateAutoInsertion(worker, languageId, getSyncUris, editor);
+    registerProviders(worker, languageId, getSyncUris, languages).catch(
+      () => {},
+    );
+    app.component("VMonacoVolar", VMonacoVolar);
+  },
+};
