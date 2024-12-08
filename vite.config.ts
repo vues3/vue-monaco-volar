bc4de22adb435f9ee9d30dc286695458fe531b8a
Jerry Bruwes <jbruwes@gmail.com>
2024-12-08 16:32:31 +0200

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,77 @@
-import type { LibraryFormats, Plugin } from "vite";
+import type { PathOrFileDescriptor } from "fs";
+import type { InputOption } from "rollup";
+import type {
+  BuildEnvironmentOptions,
+  LibraryFormats,
+  LibraryOptions,
+  Plugin,
+  PluginOption,
+} from "vite";
 
+import replace from "@rollup/plugin-replace";
 import pluginVue from "@vitejs/plugin-vue";
-import fs from "fs";
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-const apply = "build";
-const fileName = "vue-monaco-volar";
-const filePath = `./dist/${fileName}.js`;
-const writeBundle = () => {
-  fs.writeFileSync(
-    filePath,
-    `import "./${fileName}.css";\n${fs.readFileSync(filePath, "utf-8")}`,
-  );
-};
-const name = "patch-css";
-const patchCssFiles: Plugin = { apply, name, writeBundle };
-const plugins = [pluginVue(), dts(), patchCssFiles];
-const entry = "./src/index.ts";
-const formats: LibraryFormats[] = ["es"];
-const lib = { entry, fileName, formats };
-const build = { lib };
+/** Base public path when served in development or production. */
 const base = "./";
-export default defineConfig({ base, build, plugins });
+/**
+ * The name of the package file output. The default file name is the name option
+ * of the project package.json. It can also be defined as a function taking the
+ * format as an argument.
+ */
+const fileName = "vue-monaco-volar";
+/** Output format for worker bundle */
+const format: LibraryFormats = "es";
+/**
+ * Build in library mode. The value should be the global name of the lib in UMD
+ * mode. This will produce esm + cjs + umd bundle formats with default
+ * configurations that are suitable for distributing libraries.
+ */
+const lib: LibraryOptions = (() => {
+  /** Path of library entry */
+  const entry: InputOption = resolve(resolve("src"), "index.ts");
+  /** Output bundle formats */
+  const formats = [format];
+  return { entry, fileName, formats };
+})();
+/** Build specific options */
+const build: BuildEnvironmentOptions = { lib };
+const worker = (() => {
+  /** Prevents replacing strings where they are followed by a single equals sign. */
+  const preventAssignment = true;
+  /** You can separate values to replace from other options. */
+  const values = {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  };
+  /**
+   * Vite plugins that apply to worker bundle. The plugins returned by this
+   * function should be new instances every time it is called, because they are
+   * used for each rollup worker bundling process.
+   */
+  const plugins = () => [replace({ preventAssignment, values })];
+  return { format, plugins };
+})();
+const plugins: PluginOption[] = (() => {
+  /** Rollup type declaration files after emitting them. */
+  const rollupTypes = true;
+  const patchCssFiles: Plugin = (() => {
+    /** Apply the plugin only for serve or build, or on certain conditions. */
+    const apply = "build";
+    const name = "patch-css";
+    const writeBundle = () => {
+      const filePath: PathOrFileDescriptor = resolve(
+        resolve("dist", `${fileName}.js`),
+      );
+      writeFileSync(
+        filePath,
+        `import "./${fileName}.css";\n${readFileSync(filePath, "utf-8")}`,
+      );
+    };
+    return { apply, name, writeBundle };
+  })();
+  return [pluginVue(), dts({ rollupTypes }), patchCssFiles];
+})();
+export default defineConfig({ base, build, plugins, worker });
