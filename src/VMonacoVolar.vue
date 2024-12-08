2c4f1eb248bdde0a71d75ed27acc4d49fd3a45dc
Jerry Bruwes <jbruwes@gmail.com>
2024-12-08 16:32:39 +0200

    modified:   src/VMonacoVolar.vue

--- src/VMonacoVolar.vue
+++ src/VMonacoVolar.vue
@@ -7,15 +7,8 @@ import type { Ref } from "vue";
 import { registerHighlighter } from "@vues3/monaco-volar-worker/src/highlight";
 import { getOrCreateModel } from "@vues3/monaco-volar-worker/src/utils";
 import { editor, Uri } from "monaco-editor-core";
-import uuid from "uuid-random";
 import { onBeforeUnmount, onMounted, ref } from "vue";
 
-const ambiguousCharacters = false;
-const automaticLayout = true;
-const fixedOverflowWidgets = true;
-const scrollBeyondLastLine = false;
-const unicodeHighlight = { ambiguousCharacters };
-const { light: theme } = registerHighlighter();
 const props = withDefaults(
   defineProps<{
     id?: string;
@@ -23,7 +16,7 @@ const props = withDefaults(
     options: editor.IStandaloneEditorConstructionOptions;
   }>(),
   {
-    id: uuid(),
+    id: "",
     modelValue: "",
     options: () => ({}),
   },
@@ -31,53 +24,84 @@ const props = withDefaults(
 const emit = defineEmits(["update:modelValue"]);
 const monaco: Ref<HTMLElement | undefined> = ref();
 let editorInstance: editor.IStandaloneCodeEditor | undefined;
-const model = getOrCreateModel(
-  Uri.parse(`file:///${props.id}.vue`),
-  "vue",
-  await props.modelValue,
-);
-onMounted(() => {
-  if (monaco.value) {
-    editorInstance = editor.create(monaco.value, {
-      ...{
-        automaticLayout,
-        fixedOverflowWidgets,
-        model,
-        scrollBeyondLastLine,
-        theme,
-        unicodeHighlight,
-      },
-      ...props.options,
-    });
-    editorInstance.onDidChangeModelContent(() => {
-      emit("update:modelValue", editorInstance?.getValue());
-    });
-    editorInstance.focus();
-    const { _themeService: themeService } = editorInstance as unknown as Record<
-      string,
-      Record<string, Record<string, ((...args: never) => unknown) | boolean>>
-    >;
-    const { _theme: t } = themeService;
-    t.semanticHighlighting = true;
-    t.getTokenStyleMetadata = (type: string, modifiers: string[]) => {
-      let foreground = 0;
-      switch (type) {
-        case "class":
-          foreground = 11;
-          break;
-        case "function":
-        case "method":
-          foreground = 12;
-          break;
-        case "property":
-        case "variable":
-          foreground = modifiers.includes("readonly") ? 19 : 9;
-          break;
-        default:
-      }
-      return { foreground };
-    };
-  }
+onMounted(async () => {
+  editorInstance = await (async () => {
+    const automaticLayout = true;
+    const fixedOverflowWidgets = true;
+    const model = getOrCreateModel(
+      Uri.parse(`file:///${props.id}.vue`),
+      "vue",
+      await props.modelValue,
+    );
+    const scrollBeyondLastLine = false;
+    const { light: theme } = registerHighlighter();
+    const unicodeHighlight = (() => {
+      const ambiguousCharacters = false;
+      return { ambiguousCharacters };
+    })();
+    return (
+      monaco.value &&
+      editor.create(monaco.value, {
+        ...{
+          automaticLayout,
+          fixedOverflowWidgets,
+          model,
+          scrollBeyondLastLine,
+          theme,
+          unicodeHighlight,
+        },
+        ...props.options,
+      })
+    );
+  })();
+  editorInstance?.onDidChangeModelContent(() => {
+    emit("update:modelValue", editorInstance?.getValue());
+  });
+  editorInstance?.focus();
+  // const { _themeService: themeService } = editorInstance as unknown as Record<
+  //   string,
+  //   Record<string, Record<string, ((...args: never) => unknown) | boolean>>
+  // >;
+  // const { _theme: t } = themeService;
+  // t.semanticHighlighting = true;
+  // t.getTokenStyleMetadata = (type: string, modifiers: string[]) => {
+  //   let foreground = 0;
+  //   switch (type) {
+  //     case "class":
+  //       foreground = 11;
+  //       break;
+  //     case "function":
+  //     case "method":
+  //       foreground = 12;
+  //       break;
+  //     case "property":
+  //     case "variable":
+  //       foreground = modifiers.includes("readonly") ? 19 : 9;
+  //       break;
+  //     default:
+  //   }
+  //   return { foreground };
+  // };
+  // Support for semantic highlighting
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, no-underscore-dangle, @typescript-eslint/no-unsafe-assignment
+  const t = (editorInstance as any)._themeService._theme;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  t.semanticHighlighting = true;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  t.getTokenStyleMetadata = (type: string, modifiers: string[]) => {
+    switch (type) {
+      case "class":
+        return { foreground: 11 };
+      case "function":
+      case "method":
+        return { foreground: 12 };
+      case "property":
+      case "variable":
+        return { foreground: modifiers.includes("readonly") ? 19 : 9 };
+      default:
+        return { foreground: 0 };
+    }
+  };
 });
 onBeforeUnmount(() => {
   editorInstance?.dispose();
